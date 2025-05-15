4817d7dc9670f13202914c3e9c90a5f8a3f7fee4
Ivancito <tomatoeese@gmail.com>
2025-05-15 23:18:12 +0200

    Commit-4-Interface

--- Proyecto_UD_4/src/com/DAM1/Iván_Torres/interfaz/Interface.java
+++ Proyecto_UD_4/src/com/DAM1/Iván_Torres/interfaz/Interface.java
@@ -2,8 +2,11 @@ package com.DAM1.Iván_Torres.interfaz;
 
 public class Interface {
     private String name;
-
+    private String id;
+    
     public Interface(String name) {
         this.name = name;
     }
+
+
 }
